b7b1b43a057b19fae68a21b6eff208623f34397c
Laribene <l200805@dac.unicamp.br>
2020-06-19 18:48:01 -0300

    Estudos adicionais

--- Desafio 5/Complementares/products/urls.py
+++ Desafio 5/Complementares/products/urls.py
@@ -16,11 +16,11 @@
 
 from django.urls import path
 
-from products import views
+from products import views # pegar os views do proprio app
 
 urlpatterns = [
     path('list', views.list_products, name='list'),
-    #path('create', views.create_product, name='create'),
+    path('create', views.create_product, name='create'),
     #path('delete/<int:product_id>', views.delete_product, name='delete'),
     #path('update/<int:product_id>', views.update_product, name='update')
 ]
--- Desafio 5/Complementares/products/views.py
+++ Desafio 5/Complementares/products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
 from products.models import Product
+from products.forms import ProductModelForm # from pasta import classe
+
 # Create your views here.
 def list_products(request): #Sempre vai pegar uma request como argumento
     products =  Product.objects.all()
@@ -13,3 +14,10 @@ def list_products(request): #Sempre vai pegar uma request como argumento
     return render(request, 'products/list.html', context = context) # Ele reconhece a pasta template, não precisando especificar
 #Como ele vai responder uma requisição.
 #Ler o produto do banco de dados, e depois extrair os valoares da leitura e depois montar um template
+
+def create_product(request):
+    form = ProductModelForm()
+    context = { #Sempre é um dicionário
+        'form' : form
+    }
+    return render(request, 'products/create.html', context = context)
