3bcf6dbc9e02c68fc80a1eb2c7379593199f404b
Laribene <l200805@dac.unicamp.br>
2020-07-22 22:35:08 -0300

    Desafio atualizado

---
+++ Desafio 7/ExemploProfessor/products/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+from products.models import Product, Category, Order
+from django.utils.html import format_html
+from django.urls import reverse
+# Register your models here.
+
+
+class CategoryModelAdmin(admin.ModelAdmin):
+    def products(self,obj):
+        #admin:APP-NAME_MODEL-NAME_changelist
+        #APP-NAME = nome definido no apps.py
+        href = reverse('admin:products_product_changelist') + f'?category={obj.pk}'
+        return format_html(f'<a href="{href}">{obj.products.count()}</a>')
+        #Em models.py foi definido na classe Products uma relação de Category com Products
+
+    products.short_description = 'Produtos da Categoria'
+
+    list_display = ('name', 'description', 'products')
+
+class ProductModelAdmin(admin.ModelAdmin):
+    def queryset(self, request, queryset):
+        category =  request.GET.get('category')
+        if category:
+            return queryset.filter(category__pk=category)
+
+        return queryset
+
+    list_display = ('name', 'formatted_price', 'description', 'link_category')
+
+    def formatted_price(self,obj):
+        return f'{obj.price} R$'
+
+    formatted_price.short_description = 'Preço'
+
+    def link_category(self,obj):
+        href = reverse('admin:products_category_change', args=(obj.category.pk,))
+        return format_html(f'<a href="{href}">{obj.category.name}</a>')
+
+    link_category.short_description = 'Categoria'
+
+
+
+admin.site.register(Product, ProductModelAdmin)
+#Configurar a interface da admin
+admin.site.register(Category, CategoryModelAdmin)
+admin.site.register(Order)
---
+++ Desafio 7/ExemploProfessor/products/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.db.models import Q
+
+class ProductManager(models.Manager):
+    #Filtro simples
+    def with_text(self, text):
+        queryset = self.get_queryset().filter(name__contains = text)
+        return queryset
+    
+    def expensive_products(self):
+        return self.get_queryset().filter(price__gte=500)
+
+    def cheap_toys(self):
+        return self.get_queryset().filter(
+            category__name = 'Category',
+            price__lte=2
+        )
+    
+    def toys_or_expensive_item(self):
+        query_filter = Q(category__name = 'Category') | Q(price__gte = 500)
+        queryset = self.get_queryset().filter(query_filter)
+        print(queryset.query)
+        return queryset
+
+# Create your models here.
+class Category(models.Model): #Criação de alguns produtos dentro de algumas categorias
+    name = models.CharField('Nome', max_length=50)
+    description = models.TextField('Descrição')
+
+    def __str__(self):
+        return f'{self.name} - {self.products.count()}'
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+
+
+class Product(models.Model): #Criação de um produto
+    objects = ProductManager()
+    name = models.CharField('Nome', max_length=100) #max é obrigatorio
+    description = models.TextField('Descrição')
+    price = models.DecimalField('Preço', max_digits=8, decimal_places=2) #Contar 2 digitos decimais + 6 digitos antes da virgula
+    category = models.ForeignKey(Category, on_delete=models.deletion.DO_NOTHING, related_name="products") # Uma categoria pode ter varios produtos
+
+    def __str__(self): #Converter para string
+        return self.name
+
+
+class Order(models.Model):
+    name = models.CharField('Nome do cliente', max_length = 100)
+    payment = models.CharField('Meio Pagamento', max_length = 50)
+    products = models.ManyToManyField(Product)
+
+    #Informacional
+    @property
+    def total_amount(self):
+        return sum(product.price for product in self.products.all())
+
+    def __str__(self):
+        return f'{self.name} - {self.total_amount}'
