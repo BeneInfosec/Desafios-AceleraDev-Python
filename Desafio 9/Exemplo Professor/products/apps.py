247ff825c2b6111d637cd2989f5eeeefeccb089b
Laribene <l200805@dac.unicamp.br>
2020-08-17 19:52:22 -0300

    Removendo desafio 9

--- Desafio 9/Exemplo Professor/products/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProductsConfig(AppConfig):
-    name = 'products'
-
-#A maioria das vezes não mexe muito
--- Desafio 9/Exemplo Professor/rest_api/api_permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class OnlyAdminCanCreate(BasePermission):
-    def has_permission(self, request, view):
-        if request.method == "POST":
-            return request.user.is_staff #se o usuario tem a permissão de admin False or True
-        return True 
-
